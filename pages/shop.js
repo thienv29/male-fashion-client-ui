bc5f6ad5fdfd001d2f9664930fd02d54604e3f0f
thienv29 <thienv29@gmail.com>
2022-10-22 16:42:12 +0700

    Update shop.js

--- pages/shop.js
+++ pages/shop.js
@@ -190,6 +190,25 @@ export default function Shop() {
                             <div className='col-lg-9'>
                                 <div className='shop__product__option'>
                                     <div className='row'>
+                                        <div className='col-lg-6 col-md-6 col-sm-6'>
+                                            <div className='shop__product__option__left'>
+                                                <p>Showing 1–12 of 126 results</p>
+                                            </div>
+                                        </div>
+                                        <div className='col-lg-6 col-md-6 col-sm-6'>
+                                            <div className='shop__product__option__right'>
+                                                <p>Sort by Price:</p>
+                                                <select>
+                                                    <option value=''>Low To High</option>
+                                                    <option value=''>$0 - $55</option>
+                                                    <option value=''>$55 - $100</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='shop__product__option'>
+                                    <div className='row'>
 
                                         {products.map(item => {
                                             return <Product product={item} key={item._id} />;
